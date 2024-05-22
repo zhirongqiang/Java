9192134037005b0000634903c572708256ce7824
苏桓 <152363249+Dann-Zeng@users.noreply.github.com>
2024-05-23 01:11:32 +0800

    Create switch_statements.java

---
+++ switch_statements.java
@@ -0,0 +1,39 @@
+
+public class switch_statements {
+  public static void main(String[] args) {
+    System.out.println(getDayNum(0));
+  }
+
+  public static String getDayNum(int dayNum) {
+    String dayName = "";
+
+    switch (dayNum) {
+      case 0:
+        dayName = "Sunday";
+        break;
+      case 1:
+        dayName = "Monday";
+        break;
+      case 2:
+        dayName = "Tuesday";
+        break;
+      case 3:
+        dayName = "Wednsday";
+        break;
+      case 4:
+        dayName = "Thursday";
+        break;
+      case 5:
+        dayName = "Friday";
+        break;
+      case 6:
+        dayName = "Saturday";
+        break;
+      default:
+        dayName = "Invalid Day Num";
+        break;
+    }
+
+    return dayName;
+  }
+}
