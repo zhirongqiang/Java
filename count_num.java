37b182ee9887e5a14da32de8970327e6ebd511f7
苏桓 <152363249+Dann-Zeng@users.noreply.github.com>
2024-05-22 19:29:34 +0800

    Create count_num.java

---
+++ count_num.java
@@ -0,0 +1,31 @@
+package nanapig;
+
+import java.util.Scanner;
+
+public class count {
+  public static void main(String[] args) {
+    Scanner keyboardInput = new Scanner(System.in);
+    System.out.print("输入一个数字: ");
+    Double num1 = keyboardInput.nextDouble();
+
+    System.out.print("输入一个运算符: ");
+    String op = keyboardInput.next();
+
+    System.out.print("输入一个数字: ");
+    Double num2 = keyboardInput.nextDouble();
+
+    if (op.equals("+")) {
+      System.out.println(num1 + num2);
+    } else if (op.equals("-")) {
+      System.out.println(num1 - num2);
+    } else if (op.equals("*")) {
+      System.out.println(num1 * num2);
+    } else if (op.equals("/")) {
+      System.out.println(num1 / num2);
+    } else {
+      System.out.println("输入有误");
+    }
+
+    keyboardInput.close();
+  }
+}
